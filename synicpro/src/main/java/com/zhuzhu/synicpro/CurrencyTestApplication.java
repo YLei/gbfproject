bd42b833a5ee69895900d74224bb13549fbbc6f8
YLei <296759297@qq.com>
2019-12-04 16:41:37 +0800

    第一次项目提交

---
+++ synicpro/src/main/java/com/zhuzhu/synicpro/CurrencyTestApplication.java
@@ -0,0 +1,13 @@
+package com.zhuzhu.synicpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyTestApplication.class, args);
+	}
+
+}
---
+++ synicpro/src/test/java/com/zhuzhu/synicpro/CurrencyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuzhu.synicpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
