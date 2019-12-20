42a549509c2b1965ffdb9f294c8353084e3ab445
YLei <296759297@qq.com>
2019-12-20 18:04:40 +0800

    分布式通信协议--组播

---
+++ yltest/vip1219/src/main/java/socket/MulticatSocketClient.java
@@ -0,0 +1,31 @@
+package socket;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+//组播（客户端）
+public class MulticatSocketClient {
+    public static void main(String[] args) throws UnknownHostException {
+        //创建组
+        InetAddress group  = InetAddress.getByName("224.1.2.3");
+        //创建连接
+        try {
+            MulticastSocket socket = new MulticastSocket(4321);
+            //加到指定组里面
+            socket.joinGroup(group);
+            byte[] buf = new byte[256];
+            while (true){
+                DatagramPacket datagramPacket = new DatagramPacket(buf,buf.length);
+                //接收信息
+                socket.receive(datagramPacket);
+                String msg = new String(datagramPacket.getData());
+                System.out.println("接收到的数据："+msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ yltest/vip1219/src/main/java/socket/MulticatSocketServer.java
@@ -0,0 +1,35 @@
+package socket;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
+
+//组播（服务端）
+public class MulticatSocketServer {
+    public static void main(String[] args) {
+        //创建组（网段范围：224.0.0.0 ~ 239.255.255.255）
+        try {
+            InetAddress group = InetAddress.getByName("224.1.2.3");
+            //创建连接
+            MulticastSocket socket = new MulticastSocket();
+            //发送消息
+            for (int i=0;i<10;i++){
+                String data = "Hello!YL!";
+                byte[] bytes = data.getBytes();
+                //发送UDP消息
+                socket.send(new DatagramPacket(bytes,bytes.length,group,4321));
+                //休眠两秒
+                TimeUnit.SECONDS.sleep(2);
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
